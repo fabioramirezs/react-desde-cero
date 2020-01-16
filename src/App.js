0fbbf7811c69ef9f46986b2694a2f4fefcbaa2cb
Fabio Alexander Ramirez Salamanca <fabioalexander.ramirez@gmmail.com>
2020-01-16 17:50:50 -0500

    Primer Commit del Curso React Js desde Cero

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles/styles.scss";
+import Curso from "./Curso"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <>
+    <div className="main-banner img-container l-section" id="main-banner">
+      <div className="ed-grid lg-grid-6">
+        <div className="lg-cols-4 lg-x-2">
+          <img className="main-banner__img" alt="bsnner" src="https://image.winudf.com/v2/image/Y29tLndhbGxwYXBlcmNpdHkub21haXNfc2NyZWVuXzRfMTUyMjY2MDA0Ml8wNDc/screen-4.jpg?fakeurl=1&type=.jpg" />
+          <div className="main-banner__data s-center">
+            <p className="t2 s-mb-0">Cursos de EDteam</p>
+            <p> Tu futuro te espera</p>
+            <a href="http:wwww.edteam.com" className="button">Suscribirse</a>
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
-
+    <div className="ed-grid m-grid-3">
+          <Curso />
+          <Curso />
+    </div>
+</>
+)
 export default App;
