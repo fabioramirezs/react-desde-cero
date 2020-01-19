059a19908f9d479ba3bf35ce8e7c858047ecb9b8
Fabio Alexander Ramirez Salamanca <fabioalexander.ramirez@gmmail.com>
2020-01-18 19:14:32 -0500

    Recorriendo Arrays

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,37 +1,48 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const curso = {
-"profesor": "Beto Quiroga",
-"imageProfesor": "https://api.ed.team/files/avatars/38118146-4b4a-4844-8959-157614e04cd1.jpg"
-}
-
-const Curso = props => (
-<><article className="card">
+const Curso = ({title, image,price, profesor, imageProfesor}) => (
+<article className="card">
   <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-    <img src={props.image} alt={props.title}/>
+    <img src={image} alt={title}/>
   </div>
   <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
     <h3 className="t5 s-mb-2 s-center">
-     {curso.title}
+     {title}
     </h3>
     <div className="s-mb-2 s-main-center">
       <div className="card__teacher s-cross-center">
         <div className="card__avatar s-mr-1">
           <div className="circle img-container">
-            <img src={curso.imageProfesor} alt={curso.profesor}/>
+            <img src={imageProfesor} alt={profesor}/>
           </div>
         </div>
-        <span className="small">{curso.profesor}</span>
+        <span className="small">{profesor}</span>
       </div>
     </div>
     <div className="s-main-center">
-<a className="button--ghost-alert button--tiny" href="www.google.com.co">{`$ ${props.price}`} </a>
+<a className="button--ghost-alert button--tiny" href="www.google.com.co">{`$ ${price} USD`} </a>
     </div>
   </div>
 </article>
-</>
-
-
 )
+
+Curso.propTypes = {
+    title: PropTypes.string,
+    image: PropTypes.string,
+    price: PropTypes.number,
+    profesor: PropTypes.string,
+    imageProfesor: PropTypes.string
+}
+
+Curso.defaultProps = {
+    title: "No hay titulo",
+    image: "https://onlinezebra.com/wp-content/uploads/2019/01/error-404-not-found.jpg",
+    price: "--",
+    profesor: "Sin profesor",
+    imageProfesor: "https://onlinezebra.com/wp-content/uploads/2019/01/error-404-not-found.jpg"
+
+}
+
 
 export default Curso;
