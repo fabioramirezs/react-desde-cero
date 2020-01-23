55c486ed1b810362f9379812c1133be9aa931841
Fabio Alexander Ramirez Salamanca <fabioalexander.ramirez@gmmail.com>
2020-01-23 10:51:01 -0500

    Estados de la Aplicacion

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -8,34 +8,56 @@ class Formulario extends React.Component{
 
         this.state = {
             nombre: "",
-            correo: ""
+            correo: "",
+            fecha: new Date()
         }
+
+/*      this.cambiarNombre = this.cambiarNombre.bind(this)
+        this.cambiarCorreo = this.cambiarCorreo.bind(this) */
+        this.cambiarCampo = this.cambiarCampo.bind(this)
+        this.cambiarFecha = this.cambiarFecha.bind(this)
     }
+
+    cambiarCampo({value}, type){
+        this.setState({
+            [type]: value
+        })
+    }
+
+    cambiarFecha(){
+
+        this.setState({
+            fecha: new Date()
+        })
+    }
+
+/*     cambiarNombre(e){
+        this.setState({
+            nombre: e.target.value
+        })        
+    }
+    cambiarCorreo(e){
+
+        this.setState({
+            correo: e.target.value
+        })
+    } */
 
     render(){
         return (
             <div className="ed-grid"><h1>Formulario</h1>
-            
-            <form>
+            <h4>Fecha actual: {Math.ceil(this.state.fecha/1000)}</h4>
+            <form id="formulario">
                 <div className="ed-grid m-grid-2">
                 <div className="form__item">
                                     <label>Nombre Completo</label>
                                     <input type="text" 
-                                    onChange={e => {
-                                      this.setState({
-                                          nombre: e.target.value
-                                      })}
-                                    }/>
+                                    onChange={(e) => {this.cambiarCampo(e.target, "nombre")}}
+                                    />
                                 </div>       
                                 <div className="form__item">
                                     <label>Correo Electronico</label>
-                                    <input type="email" onChange={
-                                        e => {
-                                            this.setState({
-                                                correo: e.target.value
-                                            })
-                                        }
-                                    }/>
+                                    <input type="email" onChange={(e) => this.cambiarCampo(e.target, "correo")}/>
                                 </div> 
                                 {/* <div className="form__item">
                                     <input type="submit" value="Enviar" className="button full"></input>
@@ -47,5 +69,23 @@ class Formulario extends React.Component{
             </div>
         )
     }
+
+
+    componentDidMount(){
+        let elemento = document.getElementById("formulario") 
+        this.intervaloFecha = setInterval(()=> {
+            this.cambiarFecha()
+            console.log(new Date())
+        },1000)
+    }
+
+    componentDidUpdate(prevProps, prepState){
+        //console.log(prepState)
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.intervaloFecha)
+    }
+
 }
 export default Formulario
