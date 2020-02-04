3cfc93ff94a6984538deee2dddb37ca4a7e38275
Fabio Alexander Ramirez Salamanca <fabioalexander.ramirez@gmmail.com>
2020-02-04 12:05:29 -0500

    Peticiones Http Axios

--- src/components/Pages/Users.jsx
+++ src/components/Pages/Users.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Component } from 'react';
-
+import UserCard from '../Molecules/UserCard';
+import Axios from 'axios';
 
 class Users extends Component{
 
@@ -11,12 +12,43 @@ class Users extends Component{
             users: []
         }
     }
+/*     componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users', {method:'get'})
+        .then(response => response.json())
+        .then(json => {
+            this.setState({
+                users: json
+            })
+        })
+    } */
 
-    render(){
+    componentDidMount(){
+        Axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+            this.setState({
+                users: response.data
+            })    
+        })
+    }
+
+    render(){        
+        const {users} = this.state 
         return(
 
             <div className="ed-grid">
-                 <h1>Usuarios</h1>   
+                 <h1>Usuarios</h1>
+                <div className="ed-grid m-grid-3">
+                    {
+                    users.map(u => (
+                        <UserCard 
+                            key = {u.id}
+                            name = {u.name}
+                            username = {u.username}
+                            email = {u.email}                        
+                        />
+                    ))
+                    }
+                </div>
             </div>
         )
     }
